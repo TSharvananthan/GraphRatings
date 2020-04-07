96df81f4c8527bcd6b72eb0c381dd10e4829d4ef
Thanussian Sharvananthan <thanussian@gmail.com>
2020-04-07 18:07:11 -0400

    finished graph.py. may need to do small fixes otherwise

--- src/graph.py
+++ src/graph.py
@@ -1,1 +1,9 @@
+import numpy
 import matplotlib.pyplot as plt
+import pandas as pd
+
+def Graph(x, y):
+    df=pd.DataFrame({"x": x, "y": y})
+
+    plt.plot('x', 'y', data=df, linestyle='none', marker='o')
+    plt.show()
