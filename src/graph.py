cab4b92b60ddefa0a4dfcc73dca5f7d3353e046a
Thanussian Sharvananthan <thanussian@gmail.com>
2020-04-08 02:56:51 -0400

    finally got the graph working. just need to make it look prettier and fix small formatting issues

--- src/graph.py
+++ src/graph.py
@@ -11,8 +11,13 @@ def Graph(x, y, filepath, ratings):
         return OffsetImage(plt.imread(path))
 
     fig, ax = plt.subplots()
+    fig.set_size_inches((len(set(x))) + 3, (sorted(y)[-1] // 2) + 3)
+    ax.set_facecolor("tab:gray")
 
     ax.scatter(x, y)
+
+    plt.xlabel("Season #")
+    plt.ylabel("Episode #")
 
     for x0, y0 in zip(x, y):
         ab = AnnotationBbox(getImage(IMAGELIST[ratings[y0]]), (x0, y0), frameon=False)
