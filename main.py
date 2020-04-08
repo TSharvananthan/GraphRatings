191d307792b60c3f03cb99a78e6d69ae550da2ad
Thanussian Sharvananthan <thanussian@gmail.com>
2020-04-08 00:42:41 -0400

    added arguments

--- main.py
+++ main.py
@@ -5,15 +5,16 @@ from src.graph import Graph
 import argparse
 
 argObject = argparse.ArgumentParser()
-argObject.add_argument("searchterm", type=str, help="What movie are you trying to find?")
+argObject.add_argument("id", type=str, help="What movie are you trying to find?")
 argObject.add_argument("--filepath", type=str, default="output.png", help="Where would you like to save the figure")
 
 argParsed = argObject.parse_args()
 
 validateImports()
 
-data = ScrapeRatings(argParsed.searchterm)
+data = ScrapeRatings(argParsed.id)
 
+RATINGS = data[0]
 X_VALUES, Y_VALUES = data[1], data[2]
 
 Graph(X_VALUES, Y_VALUES, argParsed.filepath)
