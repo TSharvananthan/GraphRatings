70d699252fc3dda10eddf2e9be65ea2428bb2cc7
Thanussian Sharvananthan <thanussian@gmail.com>
2020-04-11 20:00:08 -0400

    added comment headers to __init__ folders

---
+++ __init__.py
@@ -0,0 +1,13 @@
+'''
+**********************************************************************
+* Project         : GraphRatings
+*
+* Author          : Thanussian Sharvananthan
+*
+* Date created    : April 11th 2020
+*
+* Purpose         : Program that takes the ID of a tv show on IMDb and
+*                   graphs the ratings of each episode by season
+*
+**********************************************************************
+'''
