9ba54b7c4d8debbeb6b83c0114c4ba33d4110376
Thanussian Sharvananthan <thanussian@gmail.com>
2020-04-11 20:03:28 -0400

    FINALLY FOUND A WAY TO GET CUSTOM X AND Y TICK LABELS. added margins

--- src/graph.py
+++ src/graph.py
@@ -5,22 +5,36 @@ from matplotlib.offsetbox import OffsetImage, AnnotationBbox
 from matplotlib.gridspec import GridSpec
 from src.imagelist import IMAGELIST
 
-def Graph(x, y, filepath, ratings):
+def Graph(x, y, filepath, ratings, coords, moviename):
     global IMAGELIST
     def getImage(path):
         return OffsetImage(plt.imread(path))
 
-    fig, ax = plt.subplots()
-    fig.set_size_inches((len(set(x))) + 3, (sorted(y)[-1] // 2) + 3)
+    ax = plt.subplots()[1]
     ax.set_facecolor("tab:gray")
 
-    ax.scatter(x, y)
+    figure = plt.gcf()
+
+    figure.set_size_inches((len(set(x))) + 3, (sorted(y)[-1] // 2) + 3)
+
+    df = pd.DataFrame({'x': x,'y': y})
 
     plt.xlabel("Season #")
     plt.ylabel("Episode #")
+    plt.title(moviename)
 
-    for x0, y0 in zip(x, y):
-        ab = AnnotationBbox(getImage(IMAGELIST[ratings[y0]]), (x0, y0), frameon=False)
+    ax.set_xticks(x)
+    ax.set_yticks(y)
+
+    plt.plot('x', 'y', data=df, linestyle='none', marker='s',markersize=10, color="green")
+
+    for place, rating in zip(coords, ratings):
+        ab = AnnotationBbox(getImage(IMAGELIST[rating]), place, frameon=False)
         ax.add_artist(ab)
 
-    plt.savefig(filepath)
+    plt.subplots_adjust(left=0.05, right=0.98, top=0.96, bottom=0.05)
+    plt.margins(x=0.05)
+    plt.margins(y=0.05)
+
+    plt.show()
+    #plt.savefig(filepath)
